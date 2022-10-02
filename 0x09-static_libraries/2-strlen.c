2724b75d644988257c0b1a5fa613a8d350c8f5a9
TheOnis <onisamuelmichael@gmail.com>
2022-10-02 08:52:08 -0700

    fix

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
- *
- * Return: The length of @str.
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
  */
-size_t strlen(const char *str)
+int _strlen(char *s)
 {
-	size_t length = 0;
+	int count = 0;
 
-	while (*str++)
-		length++;
-
-	return (length);
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
 }
